9dcb3485871f2044e7d258b2aa8d62a371b5d74b
Samuel Lubliner <74507133+Samuel-Lubliner@users.noreply.github.com>
2023-10-25 20:08:07 +0000

    added bootstrap assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "photos#index"
+
   resources :likes
   resources :follow_requests
   resources :comments
@@ -6,7 +8,4 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "photos#index"
 end
