a673201bec8c7669e6e80ff0def5e4a9ad91d6ac
Samuel Lubliner <74507133+Samuel-Lubliner@users.noreply.github.com>
2023-10-28 21:20:53 +0000

    adds followers

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get ":username/liked" => "users#liked", as: :liked 
   get ":username/feed" => "users#feed", as: :feed
-  get ":username/followers" => "users#folloers", as: :followers
+  get ":username/followers" => "users#followers", as: :followers
   get ":username/following" => "users#folloing", as: :following
   
   get ":username" => "users#show", as: :user 
